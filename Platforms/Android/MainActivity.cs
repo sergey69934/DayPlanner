83d42d6b9cf19cd3e55f0c0180f526d8cc110d00
Сергей Козлов <serzh.kozlov.4@gmail.com>
2024-12-12 13:37:43 +0700

    fix(UI): Исправлена непрозрачность status bar и подсказка у кнопки удаления заметки

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -7,5 +7,15 @@ namespace DayPlanner
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Скрыть Status Bar
+            Window.DecorView.SystemUiVisibility = Android.Views.StatusBarVisibility.Hidden;
+
+            // Полноэкранный режим
+            Window.SetFlags(Android.Views.WindowManagerFlags.Fullscreen,  Android.Views.WindowManagerFlags.Fullscreen);
+        }
     }
 }
